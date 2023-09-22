43e3d9c82a822b1a38207cbfb30ee7f213eb549b
azalllka <yunusova.azalka@gmail.com>
2023-09-22 10:33:52 +0300

    first commit

---
+++ Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Homework
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            bool f;
+            f = false;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= Math.Sqrt(n); i++)
+            {
+                if (Math.Pow(3, i) == n)
+                {
+                    f = true;
+                }
+            }
+            if (f)
+            {
+                Console.WriteLine("Число является степенью тройки");
+            }
+            else
+            {
+                Console.WriteLine("Число не является степенью тройки");
+            }
+        }
+    }
+}
+
+
